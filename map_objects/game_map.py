68b6c9d5624149a26ff404a2ca4ddb1acf5e9af4
Gerardo <heidel.gerardo@gmail.com>
2019-07-04 14:04:22 -0300

    added fake item

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -32,7 +32,7 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, max_items_per_room):
         rooms = []
         num_rooms = 0
 
@@ -63,13 +63,13 @@ class GameMap:
                     else:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
-                self.place_entities(new_room, entities, max_monsters_per_room)
+                self.place_entities(new_room, entities, max_monsters_per_room, max_items_per_room)
                 rooms.append(new_room)
                 num_rooms+=1
 
-    def place_entities(self, room, entities, max_monsters_per_room):
+    def place_entities(self, room, entities, max_monsters_per_room, max_items_per_room):
         number_of_monsters = randint(0, max_monsters_per_room)
-
+        number_of_items = randint(0, max_items_per_room)
         for i in range(number_of_monsters):
             x = randint(room.x1 + 1, room.x2 - 1)
             y = randint(room.y1 + 1, room.y2 - 1)
@@ -86,6 +86,13 @@ class GameMap:
 
                 entities.append(monster)
 
+        for i in range(number_of_items):
+            x = randint (room.x1 + 1, room.x2 - 1)
+            y = randint (room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                item = Entity(x,y, '!', libtcod.violet, 'Healing Potion', render_order = RenderOrder.ITEM )
+                entities.append(item)
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
